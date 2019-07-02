605ee6e6beba1c1846a4d10fdd475ce8e1ed794a
Michael <realq86@gmail.com>
2019-07-02 16:16:47 -0700

    1st Commit

---
+++ DoIt/DoIt/ToDoListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DoIt
+//
+//  Created by Michael on 7/2/19.
+//  Copyright © 2019 Michael. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
