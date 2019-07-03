5a4ddda0353ab633f1b5e37b07c62acdc063e1f7
Michael <realq86@gmail.com>
2019-07-02 18:12:30 -0700

    Set up basic feature of TableView UI only

--- DoIt/DoIt/ToDoListViewController.swift
+++ DoIt/DoIt/ToDoListViewController.swift
@@ -10,11 +10,52 @@ import UIKit
 
 class ToDoListViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+}
+
+
+// MARK: - UITableViewDataSource Methods
+extension ToDoListViewController: UITableViewDataSource{
+    
+    @available(iOS 2.0, *)
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    
+    @available(iOS 2.0, *)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "ChecklistItemCell", for: indexPath)
+        
+        
+        return cell
+    }
+    
+    
+}
+
+extension ToDoListViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if let cell = tableView.cellForRow(at: indexPath) {
+            cell.accessoryType = cell.accessoryType == .none ? .checkmark: .none
+        }
+        
+        
+        
+        self.tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
